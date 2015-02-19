720a720b56005f8e6e98eb88d73ddafe587a5f02
Krishna Pennacchioni <krishna@agentelinux.com.br>
2015-02-18 22:37:11 -0200

    change version 1 --> 2

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'aglx:ol3',
-  version: '0.0.1',
+  version: '0.0.2',
   // Brief, one-line summary of the package.
   summary: "Wrapper for OpenLayers 3",
   // URL to the Git repository containing the source code for this package.
