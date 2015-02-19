1aa9ddd8b6510d251df67859508207fa2c99f0cc
Krishna Pennacchioni <krishna@agentelinux.com.br>
2015-02-18 22:35:58 -0200

    change name Object OpenLayers. for ol.

--- package.js
+++ package.js
@@ -1,13 +1,19 @@
 Package.describe({
+  name: 'aglx:ol3',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
   summary: "Wrapper for OpenLayers 3",
-  version: "0.2.0",
-  name: "aglx:ol3",
-  git: "https://github.com/agentelinux/meteor-ol3"
+  // URL to the Git repository containing the source code for this package.
+  git: "https://github.com/agentelinux/meteor-ol3",
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('METEOR@0.9.0.1');
+  api.versionsFrom('1.0.3.1');
   api.add_files("ol3.fetch.json", "client");
+  api.export(['ol'], "client");
 });
 
 Package.onTest(function(api) {
